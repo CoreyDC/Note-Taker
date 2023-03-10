fcb95749fc47344f175865d21fe93aa6356b1839
Corey Calbreath <corey_calbreath@yahoo.com>
2023-03-09 18:01:03 -0600

    render both html pages

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ const PORT = 3001;
 const fs = require('fs');
 const path = require('path');
 
+// SETTING UP VIEW ENGINE FOR HTML
+app.set('view engine', 'ejs');
+
+// BRINGING IN ROUTES
+const indexRouter = require('./routes/html');
+const notesRouter = require('./routes/notes');
+
+
+// USING ROUTERS
+app.use('/', indexRouter);
+app.use('/', notesRouter)
+
+
 // INITIALIZING APP
 
 app.listen(PORT, () => {
